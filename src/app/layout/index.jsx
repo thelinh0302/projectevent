e951fa38337d86cdbe5ce19909429030c3577d55
the linh <chaulinh0302cr7@gmail.com>
2020-04-22 16:42:39 +0700

    comit1

---
+++ src/app/layout/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="h1">abc1</div>
+      </div>
+    );
+  }
+}
+
+export default App;
